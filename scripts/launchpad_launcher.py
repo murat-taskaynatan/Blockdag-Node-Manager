26c9747ab6889da6f35c23fcb2fae274716dc922
murat-taskaynatan <mtaskay@gmail.com>
2025-11-26 14:20:13 -0500

    Update launchpad default image to v0.0.3

--- scripts/launchpad_launcher.py
+++ scripts/launchpad_launcher.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from typing import Dict, List, Optional, Tuple, Union
 
 LAUNCHPAD_REPO = os.getenv("BDAG_LAUNCHPAD_REPO", "https://github.com/BlockdagNetworkLabs/blockdag-scripts.git")
-LAUNCHPAD_DEFAULT_IMAGE = os.getenv("BDAG_LAUNCHPAD_IMAGE", "blockdagnetwork/awakening:v0.0.2")
+LAUNCHPAD_DEFAULT_IMAGE = os.getenv("BDAG_LAUNCHPAD_IMAGE", "blockdagnetwork/awakening:v0.0.3")
 HELPER_TEMPLATE = Path(__file__).resolve().parent / "launchpad_entrypoint.sh"
 NETWORK_KEY_NAMES = ("network.key",)
 BDAGETH_KEY_GLOBS = ("**/bdageth/keystore/*",)
