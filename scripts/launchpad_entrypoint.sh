0ab3d836adb22eada9af016169d111eaa04462ee
murat-taskaynatan <mtaskay@gmail.com>
2025-12-01 21:58:13 -0500

    Launchpad: remove unsupported maxpendpeers flag

--- scripts/launchpad_entrypoint.sh
+++ scripts/launchpad_entrypoint.sh
@@ -47,9 +47,8 @@ fi
 
 PEER_PORT_INTERNAL="${PEER_PORT_INTERNAL:-18150}"
 MAX_PEERS="${MAX_PEERS:-100}"
-MAX_PEND_PEERS="${MAX_PEND_PEERS:-100}"
 # Always advertise matching TCP/UDP P2P ports and libp2p --port to align with host mapping.
-NODE_ARGS="$NODE_ARGS --p2ptcpport=$PEER_PORT_INTERNAL --p2pudpport=$PEER_PORT_INTERNAL --port=$PEER_PORT_INTERNAL --maxpeers=$MAX_PEERS --maxpendpeers=$MAX_PEND_PEERS"
+NODE_ARGS="$NODE_ARGS --p2ptcpport=$PEER_PORT_INTERNAL --p2pudpport=$PEER_PORT_INTERNAL --port=$PEER_PORT_INTERNAL --maxpeers=$MAX_PEERS"
 
 # If EXTERNAL_IP is provided and not already present, advertise it.
 if [ -n "${EXTERNAL_IP:-}" ] && ! printf '%s' "$NODE_ARGS" | grep -q -- '--externalip='; then
